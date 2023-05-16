d9f424398f9e4aea334c5148ef0f87818066296c
Rahul Kumar <rahulloni000@gmail.com>
2023-05-16 08:08:12 +0545

    my adapter

--- app/src/main/java/com/example/recycler_view/MyAdapter.java
+++ app/src/main/java/com/example/recycler_view/MyAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,6 +27,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
      holder.mtitle.setText(listData[position].getTitle());
      holder.mDesc.setText(listData[position].getDescription());
      holder.itemImage.setImageResource(listData[position].getImg());
+
+     holder.itemView.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             Toast.makeText(mContext, "Click Me a", Toast.LENGTH_SHORT).show();
+         }
+     });
     }
 
     @Override
