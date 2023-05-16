a4475e9e6e778e86dac24f291ad2e6787f9c7bc3
Rahul Kumar <rahulloni000@gmail.com>
2023-05-16 08:06:03 +0545

    My Adapter

--- app/src/main/java/com/example/recycler_view/MainActivity.java
+++ app/src/main/java/com/example/recycler_view/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.recycler_view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
     }
+    public void init(){
+        recyclerView=findViewById(R.id.recycler);
+    }
+
 }
--- app/src/main/java/com/example/recycler_view/MyAdapter.java
+++ app/src/main/java/com/example/recycler_view/MyAdapter.java
@@ -1,13 +1,48 @@
 package com.example.recycler_view;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MyAdapter extends RecyclerView {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
     private MyList[] listData;
-    public MyAdapter(@NonNull Context context) {
-        super(context);
+    private Context mContext;
+
+    @NonNull
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_data,parent);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+     holder.mtitle.setText(listData[position].getTitle());
+     holder.mDesc.setText(listData[position].getDescription());
+     holder.itemImage.setImageResource(listData[position].getImg());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView itemImage;
+        TextView mtitle,mDesc;
+        public ViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+             itemImage=itemView.findViewById(R.id.img);
+             mtitle=itemView.findViewById(R.id.title);
+             mDesc=itemView.findViewById(R.id.description);
+
+        }
     }
 }
